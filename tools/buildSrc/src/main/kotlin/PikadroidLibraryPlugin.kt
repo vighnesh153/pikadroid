6f104eeabcad6d1183365ce22f4b16b3703c0214
vighnesh153 <pikachu@vighnesh153.dev>
2025-08-31 23:17:23 +0530

    configure automated runs for android instrumentation tests

--- tools/buildSrc/src/main/kotlin/PikadroidLibraryPlugin.kt
+++ tools/buildSrc/src/main/kotlin/PikadroidLibraryPlugin.kt
@@ -36,6 +36,34 @@ class PikadroidLibraryPlugin : Plugin<Project> {
                     )
                 }
             }
+
+            testOptions {
+                // Gradle managed devices are not yet stable. Running tests on them fails because of
+                // some random issues like failed to pull system image, not enough memory available, etc.
+                managedDevices {
+                    val pikadroidTestDevices = listOf(
+                        localDevices.create("pixel_9_pro_xl_api_36") {
+                            device = "Pixel 9 Pro XL"
+                            sdkVersion = 36
+                            systemImageSource = "google"
+                        },
+                        localDevices.create("pixel_7a_api_31") {
+                            device = "Pixel 7a"
+                            sdkVersion = 31
+                            systemImageSource = "google"
+                        },
+                        localDevices.create("small_phone_api_24") {
+                            device = "Small Phone"
+                            sdkVersion = 24
+                            systemImageSource = "google"
+                        }
+                    )
+
+                    groups.create("pikadroidTestDevices") {
+                        targetDevices.addAll(pikadroidTestDevices)
+                    }
+                }
+            }
         }
     }
 }
